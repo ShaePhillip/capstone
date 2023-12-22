e736b18bfb683c29e029ca396e0a57ec02a6df63
Shae <montague.shae@gmail.com>
2023-12-22 10:51:07 +0200

    Docstrings added to views.py files

--- bandsite/views.py
+++ bandsite/views.py
@@ -3,10 +3,21 @@ from bandsite.models import Song
 from django.contrib.auth.decorators import login_required
 
 def home(request):
+    """
+    Render the home page.
+
+    Returns:
+        HttpResponse: Rendered home page containing information about the band.
+    """
     return render(request, 'bandsite/home.html')
 
 def songs_list(request):
-    # Retrieve all songs from the database
+    """
+    Display a list of songs grouped by album.
+
+    Returns:
+        HttpResponse: Rendered page showing a list of songs grouped by album.
+    """
     songs_list = Song.objects.all()
 
     # Pass the songs_list to the template for rendering
@@ -14,5 +25,17 @@ def songs_list(request):
 
 @login_required(login_url='user_auth:login')
 def user_profile(request):
+    """
+    Render the user profile page for authenticated users.
+
+    This view requires users to be logged in. If the user is not authenticated,
+    they will be redirected to the login page.
+
+    Args:
+        request (HttpRequest): The HTTP request.
+
+    Returns:
+        HttpResponse: Rendered user profile page for authenticated users.
+    """
     return render(request, 'bandsite/user_profile.html')
 
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,17 +1,25 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 from django.http import HttpResponse
-# from .forms import CustomUserCreationForm  # Updated import
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-
 def signup(request):
+    """
+    Handle user registration.
+
+    If the HTTP method is POST, attempt to create a new user based on the provided
+    username, email, and password. Check if the username already exists. If the
+    registration is successful, redirect to the signup page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response for the signup page.
+    """
     if request.method == 'POST':
         # Get user data from the POST request
         username = request.POST['username']
@@ -27,6 +35,19 @@ def signup(request):
     return render(request, 'user_auth/signup.html')
 
 def user_login(request):
+    """
+    Handle user login.
+
+    If the HTTP method is POST, validate the user login form. If the form is valid,
+    log in the user and redirect to the user profile page. If the form is not valid,
+    render the login page with error messages.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response for the login page.
+    """
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -38,10 +59,32 @@ def user_login(request):
     return render(request, 'user_auth/login.html', {'form': form})
 
 def user_logout(request):
+    """
+    Handle user logout.
+
+    Log out the currently authenticated user and redirect to the home page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponseRedirect: Redirect to the home page after logout.
+    """
     logout(request)
     return redirect('bandsite:home')  # Redirect to the home page after logout
 
 class SignUpView(generic.CreateView):
+    """
+    Handle user registration using Django's generic CreateView.
+
+    Attributes:
+        form_class (UserCreationForm): The form class for user registration.
+        success_url (str): The URL to redirect to after successful registration.
+        template_name (str): The name of the template to render for registration.
+
+    Note:
+        Inherits from Django's generic CreateView to simplify user registration.
+    """
     form_class = UserCreationForm
     success_url = reverse_lazy("user_auth:login")
     template_name = "user_auth/signup.html"
